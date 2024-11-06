34f169c55e4c110b660fd88b4b1bad435b64296b
Minzhi <quminzhi@gmail.com>
2024-11-06 08:46:21 +0000

    Updated install.sh and tmux.conf for vm

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ BASEDIR=$(dirname "$0")
 
 cd "$BASEDIR" || exit 2
 
-ln -s "${PWD}/zshrc" ~/.zshrc 2>/dev/null || true
+#ln -s "${PWD}/zshrc" ~/.zshrc 2>/dev/null || true
 ln -s "${PWD}/vimrc" ~/.vimrc 2>/dev/null || true
 ln -s "${PWD}/gitconfig" ~/.gitconfig 2>/dev/null || true
 ln -s "${PWD}/gitignore" ~/.gitignore 2>/dev/null || true
-ln -s "${PWD}/p10k.zsh" ~/.p10k.zsh 2>/dev/null || true
+#ln -s "${PWD}/p10k.zsh" ~/.p10k.zsh 2>/dev/null || true
 ln -s "${PWD}/tmux.conf" ~/.tmux.conf 2>/dev/null || true
